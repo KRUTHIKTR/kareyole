f76f72716c22313f0136142a6dbf4e174a4b0b8f
bhavishyashetty <bhavishyashetty2004@gmail.com>
2024-03-14 16:32:19 +0530

    .

--- frontend/register/register1.js
+++ frontend/register/register1.js
@@ -1,7 +1,9 @@
-const express = require('express');
-const mysql = require('mysql2');
-const bodyParser = require('body-parser');
-const router = express.Router([options])
+// 
+import express from 'express';
+import mysql from 'mysql2';
+import bodyParser from 'body-parser';
+
+// const router = express.Router([mergeParams]);
 const app = express();
 app.use(express.static(path.join(__dirname, 'register')));
 
@@ -12,10 +14,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: 'password',
+  password: 'Ashadu@0403',
   database: 'kareyole'
 });
-
 const options = {
   dotfiles: 'ignore',
   etag: false,
@@ -37,8 +38,9 @@ pool.getConnection((err, connection) => {
     return;
   }
 
-  app.post('/register', (req, res) => {
-    const { name, email, password } = req.body;
+  app.get('/register.html', (req, res) => { 
+    
+    const { name, user_id, password } = req.body;
 
     // Check if the email already exists
     pool.query('SELECT * FROM users WHERE user_id = ?', [user_id], (error, results) => {
@@ -50,7 +52,7 @@ pool.getConnection((err, connection) => {
           res.status(400).json({ message: 'Email already exists' });
         } else {
           // Email doesn't exist, insert new user into the database
-          pool.query('INSERT INTO users ( user_id,name, password) VALUES (?, ?, ?)', [ user_id,name, password], (error, results) => {
+          pool.query('INSERT INTO users ( user_id,name, password) ALUES (?, ?, ?)', [ user_id,name, password], (error, results) => {
             if (error) {
               res.status(500).json({ message: 'Internal Server Error' });
             } else {
